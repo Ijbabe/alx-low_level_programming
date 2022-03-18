9a63a36107ccbf736a4f60a7d3ca391fe1cf8154
Ijbabe <ijeomakalu13@gmail.com>
2022-03-17 18:02:20 -0700

    task 2 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,10 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - test function that prints if integer is positive or negative
- * Return: 0
- */
+#include <stdio.h>
 
-int main(void)
-{
-	int i;
-	i = 98;
-	positive_or_negative(i);
+void positive_or_negative(int i);
 
-	return (0);
-}
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H */

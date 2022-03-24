8d4aafb367ffff55b2b7638cc9bdc66852d47ece
Ijbabe <ijeomakalu13@gmail.com>
2022-03-24 08:26:23 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat- concatenates two strings.
+ * @dest: destination
+ *
+ * @src: source.
+ *
+ * Return: destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char dest;
+	char src;
+
+	strcat(destination, source);
+
+	return (dest);
+}

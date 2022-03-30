40d5f79bb4464e54bd502d4a01abe2f8318dfc7d
Ijbabe <ijeomakalu13@gmail.com>
2022-03-30 18:57:12 +0000

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,18 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	int a = 0;
-
-	if (*(s + a) < '\0')
+	if (*s < '\0')
 	{
-		_putchar(*(s + a));
-		a++;
-		_puts_recursion(s + a);
-		a++;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
 		_putchar('\n');
 	}
-	return (s);
 }

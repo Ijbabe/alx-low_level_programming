ef0fb16b7a3fb614925a4157465f5672a19b7414
Ijbabe <ijeomakalu13@gmail.com>
2022-04-02 08:08:41 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdiio.h>
+
+/**
+ * main- prints sum of positive integers
+ * @argc: count arguments
+ *
+ * @argv: array
+ *
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum; //a for loop
+
+	for (i = 1; i < argc; i++)
+	{
+		{
+			for (b = 0; argv[a][b] != '\0'; b++)
+			if ( !isdigit = argv[a][b])
+				printf("Error\n");
+		return (1);
+		}
+		sum = 0;
+		if (argc < 2)
+			printf("0\n");
+		else
+			sum += atoi(argv[a]);
+		printf("%d\n", sum);
+	}
+	return (0);
+}
+		
+

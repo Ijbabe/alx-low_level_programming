0971d5ed4af4bd4fb713ff3f778b348a0d87bc73
Ijbabe <ijeomakalu13@gmail.com>
2022-04-07 08:27:05 -0700

    task 0

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strncpy - function
+ * @dest: string
+ *
+ * @src: string
+ *
+ * @n: integer
+ *
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}

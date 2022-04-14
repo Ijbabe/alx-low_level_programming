9f0e8995efc83885f475ded00962c564aea79310
Ijbabe <ijeomakalu13@gmail.com>
2022-04-14 16:06:35 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints alphabet in lowercase 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 0;
+	char alpha = 'a';
+
+	for (a = 0; a <= 10; a++)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}

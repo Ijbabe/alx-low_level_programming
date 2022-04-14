33b045839a9291ee3694fed2d1a9e785b470adeb
Ijbabe <ijeomakalu13@gmail.com>
2022-04-14 16:11:51 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int a = 0;
 	char alpha = 'a';
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		while (alpha <= 'z')
 		{
@@ -18,6 +18,6 @@ void print_alphabet_x10(void)
 			alpha++;
 		}
 		_putchar('\n');
+		a++;
 	}
-	a++;
 }

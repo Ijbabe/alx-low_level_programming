77442fc2d97301c4eced7647997952e90cdedbdb
Ijbabe <ijeomakalu13@gmail.com>
2022-04-15 14:21:37 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,13 +19,15 @@ void times_table(void)
 			num = row * col;
 			if ((num / 10) > 0)
 				_putchar((num / 10) + '0');
+			else
+				_putchar(' ');
+			_putchar((num % 10) + '0');
 		}
 		if (col < 9)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
-		_putchar((num % 10) + '0');
 		_putchar('\n');
 	}
 }

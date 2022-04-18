9b2a263ca3405866d1e86cef54032a840a455749
Ijbabe <ijeomakalu13@gmail.com>
2022-04-18 05:44:46 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog- structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: for struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif

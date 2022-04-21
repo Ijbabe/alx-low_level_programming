5f21174601d5fbc27a4b699ffc90d7c8fd7cd7df
Ijbabe <ijeomakalu13@gmail.com>
2022-04-21 04:14:02 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,8 +17,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	char *done;
 
-	if (separator == NULL)
-		return;
 	va_start(pr, n);
 	for (i = 0; i < n; i++)
 	{

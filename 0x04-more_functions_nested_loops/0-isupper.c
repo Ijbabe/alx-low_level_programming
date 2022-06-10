fa2c6fcf2e917d3df65e2d5b3f0e57349476b166
Ijbabe <ijeomakalu13@gmail.com>
2022-06-09 20:29:25 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper- checks for uppercase
+ * @c: character
+ *
+ * Description: character check
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	char up = '65';
+
+	for (; up <= '90'; up++)
+	{
+		if (c == up)
+			return (1);
+		else
+			return (0);
+	}
+}

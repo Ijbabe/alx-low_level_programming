8f37b5c59ae433f29471da2e9c808baef535872d
Ijbabe <ijeomakalu13@gmail.com>
2022-06-13 13:23:29 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string- reverse a string
+ * @s: string
+ *
+ * Description: a string
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int a = 0;
+
+	while (s[a] != 0)
+	{
+		a++;
+	}
+	a--;
+}
